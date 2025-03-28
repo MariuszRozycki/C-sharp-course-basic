befd1d09b0007baf81c8b951f852181a51e9a3ad
MariuszRozycki <mariansc83@icloud.com>
2025-03-28 13:56:42 +0100

    Add switch statement

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -9,31 +9,22 @@ namespace FirstProject
 
         static void Main()
         {
-            Console.WriteLine("Type in your name:");
-
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Hello");
-            Console.WriteLine(name);
-
-            string someText = "Some text";
-
-            char jChar = 'j';
-            char jCharUnicode = '\u006A';
-
-            bool isUserReady = true;
-
-            DateTime now = DateTime.Now;
-
-            Console.WriteLine(now.ToString());
-            DateTime dateOfBirth = new DateTime(1983, 4, 20);
-
-            Console.WriteLine(dateOfBirth.ToString());
-
-            byte byteNumber = 200;
-            float floatNumber = 1.5F;
-            decimal decimalNumber = 1.5M;
-            double doubleNumber = 1.5;
+            switch(DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("It's Monday - first day of week...");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("It's Friday - the last day of the work week!");
+                    break;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("It's a weekend!");
+                    break;
+                default:
+                    Console.WriteLine("The middle of the week");
+                    break;
+            }
+        }        
     }
-}
 }
