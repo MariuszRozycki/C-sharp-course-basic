b6f61daa5e8d0035c83ba85a60d4e08525d5b9b9
MariuszRozycki <mariansc83@icloud.com>
2025-03-30 19:05:21 +0200

    02-10while-loop

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,39 +1,25 @@
 ﻿using System;
+using System.Text;
 
 namespace FirstProject
-
 {
-
     class Program
     {
-
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Type in your name:");
+            string[] cars = { "Volvo", "BMW", "Mazda" };
 
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Hello");
-            Console.WriteLine(name);
-
-            string someText = "Some text";
-
-            char jChar = 'j';
-            char jCharUnicode = '\u006A';
-
-            bool isUserReady = true;
-
-            DateTime now = DateTime.Now;
-
-            Console.WriteLine(now.ToString());
-            DateTime dateOfBirth = new DateTime(1983, 4, 20);
-
-            Console.WriteLine(dateOfBirth.ToString());
-
-            byte byteNumber = 200;
-            float floatNumber = 1.5F;
-            decimal decimalNumber = 1.5M;
-            double doubleNumber = 1.5;
+            int i = 0;
+            while (i < cars.Length)
+            {
+                Console.WriteLine(cars[i]);
+                if (cars[i] == "BMW")
+                {
+                    Console.WriteLine("Bye");
+                    break;
+                }
+                i++;
+            }
+        }
     }
-}
 }
