b679ef776021864ec9de225c1ff8c65bf76f220a
MariuszRozycki <mariansc83@icloud.com>
2025-03-31 12:15:13 +0200

    Add namespaces

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FirstProject.Enums;
 
 namespace FirstProject
 
@@ -9,31 +10,7 @@ namespace FirstProject
 
         static void Main()
         {
-            Console.WriteLine("Type in your name:");
-
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Hello");
-            Console.WriteLine(name);
-
-            string someText = "Some text";
-
-            char jChar = 'j';
-            char jCharUnicode = '\u006A';
-
-            bool isUserReady = true;
-
-            DateTime now = DateTime.Now;
-
-            Console.WriteLine(now.ToString());
-            DateTime dateOfBirth = new DateTime(1983, 4, 20);
-
-            Console.WriteLine(dateOfBirth.ToString());
-
-            byte byteNumber = 200;
-            float floatNumber = 1.5F;
-            decimal decimalNumber = 1.5M;
-            double doubleNumber = 1.5;
+            Gender gender = Gender.Male;
+        }
     }
-}
 }
