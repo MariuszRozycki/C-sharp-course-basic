ba831ab9f7acbc6e0472aa46ebfcb5ca0c18c73e
MariuszRozycki <mariansc83@icloud.com>
2025-03-31 11:45:46 +0200

    Add try-catch-finally block

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -9,31 +9,27 @@ namespace FirstProject
 
         static void Main()
         {
-            Console.WriteLine("Type in your name:");
+            string[] cars = { "Volvo", "BMW", "Mazada" };
 
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Hello");
-            Console.WriteLine(name);
-
-            string someText = "Some text";
-
-            char jChar = 'j';
-            char jCharUnicode = '\u006A';
-
-            bool isUserReady = true;
-
-            DateTime now = DateTime.Now;
-
-            Console.WriteLine(now.ToString());
-            DateTime dateOfBirth = new DateTime(1983, 4, 20);
-
-            Console.WriteLine(dateOfBirth.ToString());
-
-            byte byteNumber = 200;
-            float floatNumber = 1.5F;
-            decimal decimalNumber = 1.5M;
-            double doubleNumber = 1.5;
+            try
+            {
+                Console.WriteLine("Inside try - 1");
+                cars[4] = "Tesla";   // code that might thrown exeption
+                Console.WriteLine("Inside try - 2");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("HandlingIndexOutOfRangeException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Handling any exception");
+            }
+            finally
+            {
+                Console.WriteLine("CleanUp");
+            }
+            Console.WriteLine("Outside of try-catch");
+        }
     }
-}
 }
