9fe0f05528b2d068e0f540993b9197441366212e
MariuszRozycki <mariansc83@icloud.com>
2025-03-31 11:21:30 +0200

    Add nullable-types

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -9,31 +9,9 @@ namespace FirstProject
 
         static void Main()
         {
-            Console.WriteLine("Type in your name:");
+            int? favoriteNumber = default;
 
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Hello");
-            Console.WriteLine(name);
-
-            string someText = "Some text";
-
-            char jChar = 'j';
-            char jCharUnicode = '\u006A';
-
-            bool isUserReady = true;
-
-            DateTime now = DateTime.Now;
-
-            Console.WriteLine(now.ToString());
-            DateTime dateOfBirth = new DateTime(1983, 4, 20);
-
-            Console.WriteLine(dateOfBirth.ToString());
-
-            byte byteNumber = 200;
-            float floatNumber = 1.5F;
-            decimal decimalNumber = 1.5M;
-            double doubleNumber = 1.5;
+            Console.WriteLine("Favorite number: " + (favoriteNumber.HasValue ? favoriteNumber : ""));
     }
 }
 }
